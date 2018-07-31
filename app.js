12c7a8fdb636a02246fe21899e1ea0111c0abd01
Matthew Rappaport <mrappaport@spinvfx.com>
2018-07-31 11:09:21 -0400

    Added random life total colours

---
+++ app.js
@@ -0,0 +1,6 @@
+// Randomly assign colours to the life totals
+document.getElementById('life1').style.backgroundColor =
+  'hsl(' + ((Math.floor(Math.random() * 12)) * 30) + ', 50%, 50%)'
+
+document.getElementById('life2').style.backgroundColor =
+  'hsl(' + ((Math.floor(Math.random() * 12)) * 30) + ', 50%, 50%)'
