cf7c7d7e4d6ddf3596204b9e00d77b4da6016b93
Matthew Rappaport <mrappaport@spinvfx.com>
2018-08-07 14:59:35 -0400

    Added File IO to the App and Viewer with Updated XML

--- app.js
+++ app.js
@@ -1,22 +1,49 @@
-// Randomly assign colours to the life totals
-var p1Color = (Math.floor(Math.random() * 12)) * 30
-var p2Color = (Math.floor(Math.random() * 12)) * 30
+function XMLLoad () {
+  var xhttp = new XMLHttpRequest()
+  // Set up what to do when the state changes of the GET
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) { // If complete
+      // Parse out the XML into players
+      var xmlDoc = this.responseXML
+      var p1 = xmlDoc.getElementsByTagName('player1')[0]
+      var p2 = xmlDoc.getElementsByTagName('player2')[0]
 
-document.getElementById('life1').style.backgroundColor = 'hsl(' + p1Color + ', 50%, 50%)'
-document.getElementById('gain1').style.backgroundColor = 'hsl(' + p1Color + ', 50%, 40%)'
-document.getElementById('lose1').style.backgroundColor = 'hsl(' + p1Color + ', 50%, 40%)'
+      // Deal with Player 1
+      document.getElementById('life1').style.backgroundColor =
+        'hsl(' + p1.getElementsByTagName('color')[0].textContent + ', 50%, 50%)'
+      document.getElementById('gain1').style.backgroundColor =
+        'hsl(' + p1.getElementsByTagName('color')[0].textContent + ', 50%, 40%)'
+      document.getElementById('lose1').style.backgroundColor =
+        'hsl(' + p1.getElementsByTagName('color')[0].textContent + ', 50%, 40%)'
+      document.getElementById('lp1').textContent =
+        p1.getElementsByTagName('life')[0].textContent
 
-document.getElementById('life2').style.backgroundColor = 'hsl(' + p2Color + ', 50%, 50%)'
-document.getElementById('gain2').style.backgroundColor = 'hsl(' + p2Color + ', 50%, 40%)'
-document.getElementById('lose2').style.backgroundColor = 'hsl(' + p2Color + ', 50%, 40%)'
+      // Deal with Player 2
+      document.getElementById('life2').style.backgroundColor =
+        'hsl(' + p2.getElementsByTagName('color')[0].textContent + ', 50%, 50%)'
+      document.getElementById('gain2').style.backgroundColor =
+        'hsl(' + p2.getElementsByTagName('color')[0].textContent + ', 50%, 40%)'
+      document.getElementById('lose2').style.backgroundColor =
+        'hsl(' + p2.getElementsByTagName('color')[0].textContent + ', 50%, 40%)'
+      document.getElementById('lp2').textContent =
+        p2.getElementsByTagName('life')[0].textContent
+      console.log('Loaded XML')
+    }
+  }
+  // Create and Send the request
+  xhttp.open('GET', 'data.xml?' + Math.random(), true) // Use the Random to stop cache
+  xhttp.send()
+}
 
 // Handle Buttons
 function gain(x) {
-	var l = document.getElementById('lp' + x)
-	l.textContent = parseInt(l.textContent) + 1
+  var l = document.getElementById('lp' + x)
+  l.textContent = parseInt(l.textContent) + 1
 }
 
 function lose(x) {
-	var l = document.getElementById('lp' + x)
-	l.textContent = parseInt(l.textContent) - 1
+  var l = document.getElementById('lp' + x)
+  l.textContent = parseInt(l.textContent) - 1
 }
+
+XMLLoad()
--- viewer.js
+++ viewer.js
@@ -1,22 +1,65 @@
-// Randomly Determine their colours
-var p1Color = Math.floor(Math.random() * 12) * 30
-var p2Color = Math.floor(Math.random() * 12) * 30
+function XMLLoad () {
+  var xhttp = new XMLHttpRequest()
+  // Set up what to do when the state changes of the GET
+  xhttp.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) { // If complete
+      // Parse out the XML into players
+      var xmlDoc = this.responseXML
+      var p1 = xmlDoc.getElementsByTagName('player1')[0]
+      var p2 = xmlDoc.getElementsByTagName('player2')[0]
+      var m
 
-console.log(p1Color, p2Color)
+      // Deal with Player 1
+      document.getElementById('info1').style.backgroundColor =
+        'hsl(' + p1.getElementsByTagName('color')[0].textContent + ', 33%, 49%)'
+      document.getElementById('player1').style.backgroundColor =
+        'hsl(' + p1.getElementsByTagName('color')[0].textContent + ', 43%, 61%)'
+      document.getElementById('name1').textContent =
+        p1.getElementsByTagName('name')[0].textContent
+      document.getElementById('record1').textContent =
+        p1.getElementsByTagName('record')[0].textContent
+      document.getElementById('life1').textContent =
+        p1.getElementsByTagName('life')[0].textContent
+      loadGame(p1, 1, 1)
+      loadGame(p1, 1, 2)
 
-// Assign those colours to the elements
-document.getElementById('info1').style.backgroundColor =
-  'hsl(' + p1Color + ', 33%, 49%)'
-document.getElementById('player1').style.backgroundColor =
-  'hsl(' + p1Color + ', 43%, 61%)'
+      // Deal with Player 2
+      document.getElementById('info2').style.backgroundColor =
+        'hsl(' + p2.getElementsByTagName('color')[0].textContent + ', 33%, 49%)'
+      document.getElementById('player2').style.backgroundColor =
+        'hsl(' + p2.getElementsByTagName('color')[0].textContent + ', 43%, 61%)'
+      document.getElementById('life2').textContent =
+        p2.getElementsByTagName('life')[0].textContent
+      document.getElementById('name2').textContent =
+        p2.getElementsByTagName('name')[0].textContent
+      document.getElementById('record2').textContent =
+        p2.getElementsByTagName('record')[0].textContent
+      document.getElementById('life2').textContent =
+        p2.getElementsByTagName('life')[0].textContent
+      loadGame(p2, 2, 1)
+      loadGame(p2, 2, 2)
+    }
+  }
+  // Create and Send the request
+  xhttp.open('GET', 'data.xml?' + Math.random(), true) // Use the Random to stop cache
+  xhttp.send()
+}
 
-document.getElementById('info2').style.backgroundColor =
-  'hsl(' + p2Color + ', 33%, 49%)'
-document.getElementById('player2').style.backgroundColor =
-  'hsl(' + p2Color + ', 43%, 61%)'
+function loadGame (player, p, match){
+  var stat = player.getElementsByTagName('m' + match)[0].textContent
+  console.log(stat)
+  var newClass
+  // Determine the New Class
+  switch (stat) {
+    case "1":
+      newClass = 'mwinIndicatorF'
+      break
+    default:
+      newClass = 'mwinIndicator'
+      break
+  }
+  // Apply the New Class
+  document.getElementById('mwins' + p).children[match - 1].className = newClass
+}
 
-// Set flag for Perfect
-var l1 = document.getElementById('life1')
-l1.style['text-shadow'] = '4px 4px gold'
-var l2 = document.getElementById('life2')
-l2.style['text-shadow'] = '4px 4px gold'
+XMLLoad()
